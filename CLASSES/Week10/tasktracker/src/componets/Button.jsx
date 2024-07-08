0727478fd1a42292b829eacd066191b4517573a6
Logan Oram <logan.oram@keyin.com>
2024-07-08 10:09:10 -0230

    July8

--- CLASSES/Week10/tasktracker/src/componets/Button.jsx
+++ CLASSES/Week10/tasktracker/src/componets/Button.jsx
@@ -1,9 +1,23 @@
-function Button({ text, color }) {
-    return (
-      <button style={{ backgroundColor: color }} className="btn">
-        {text}
-      </button>
-    );
-  }
-  
-  export default Button;
+import PropTypes from "prop-types";
+import Button from "./Button";
+
+function Header({ title }) {
+  const onClick = () => {
+    console.log("Click");
+  };
+  return (
+    <header>
+      <h1>{title}</h1>
+      <Button text="Add" color="green" onClick={onClick} />
+    </header>
+  );
+}
+
+Header.defaultProps = {
+  title: "Task Tracker!",
+};
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
+};
+export default Header;
