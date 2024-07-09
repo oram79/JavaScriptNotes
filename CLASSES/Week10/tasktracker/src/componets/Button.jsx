762109b8cb7ee97f00c108c96ec7cee9f9d4f987
Logan Oram <logan.oram@keyin.com>
2024-07-08 23:16:59 -0230

    bugFixes

--- CLASSES/Week10/tasktracker/src/componets/Button.jsx
+++ CLASSES/Week10/tasktracker/src/componets/Button.jsx
@@ -1,23 +1,13 @@
-import PropTypes from "prop-types";
-import Button from "./Button";
-
-function Header({ title }) {
-  const onClick = () => {
-    console.log("Click");
-  };
+function Button({ text, color, onClick }) {
   return (
-    <header>
-      <h1>{title}</h1>
-      <Button text="Add" color="green" onClick={onClick} />
-    </header>
+    <button
+      style={{ backgroundColor: color }}
+      className="btn"
+      onClick={onClick}
+    >
+      {text}
+    </button>
   );
 }
 
-Header.defaultProps = {
-  title: "Task Tracker!",
-};
-
-Header.propTypes = {
-  title: PropTypes.string.isRequired,
-};
-export default Header;
+export default Button;
