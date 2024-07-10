964e7dd7118af250b3532c6131dbdb28efff3f7b
Logan Oram <logan.oram@keyin.com>
2024-07-10 12:09:15 -0230

    July10

--- CLASSES/Week10/tasktracker/src/App.js
+++ CLASSES/Week10/tasktracker/src/App.js
@@ -1,14 +1,43 @@
 import "./App.css";
-import Header from "./componets/Header";
-import Tasks from "./componets/Tasks";
-import Test2 from "./componets/Test2";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { id: 1, text: "Appointment", day: "July 20,2024 at 1pm", reminder: true },
+    { id: 2, text: "Meeting", day: "July 21,2024 at 10 am", reminder: false },
+    { id: 3, text: "GYM", day: "August 01,2024 at 11 pm", reminder: true },
+  ]);
+
+  // to delete a task
+
+  const deleteTask = (id) => {
+    // console.log("delete", id);
+
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // to toggleReminder true or false
+
+  const toggleReminder = (id) => {
+    console.log("doble click working");
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
     <div className="container">
-      {/* <Header title="Task Tracker!" />
-      <Tasks /> */}
-      <Test2 />
+      <Header title="Task Tracker!" />
+
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No tasks to show"
+      )}
     </div>
   );
 }
